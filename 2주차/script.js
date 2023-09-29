0067cc974df3babbda4be90421d65bad1a1ae176
Minkyung Ko <komg001234@gmail.com>
2023-09-30 03:52:04 +0900

    Add button click event

--- 2주차/script.js
+++ 2주차/script.js
@@ -4,12 +4,16 @@ let email = document.getElementById("email");
 let password = document.getElementById("password");
 let passwordCheck = document.getElementById("passwordCheck");
 let isValid = [false, false, false, false, false];
+let button = document.getElementById("signUpBtn");
+let modal = document.getElementById("modalWrap");
+let closeButton = document.getElementById("closeButton");
 
 uname.addEventListener("keyup", function () {
   let nameError = document.getElementById("nameError");
   if (uname.value === "") {
     nameError.innerHTML = "필수 입력 항목입니다!";
     nameError.style.color = "red";
+    isValid[0] = false;
   } else {
     nameError.innerHTML = "멋진 이름이네요!";
     nameError.style.color = "green";
@@ -23,6 +27,7 @@ nickname.addEventListener("keyup", function () {
   if (nickname.value.length < 2 || nickname.value.length > 5) {
     nicknameError.innerHTML = "닉네임은 2~5글자로 구성해주세요!";
     nicknameError.style.color = "red";
+    isValid[1] = false;
   } else {
     nicknameError.innerHTML = "멋진 닉네임이군요!";
     nicknameError.style.color = "green";
@@ -36,6 +41,7 @@ email.addEventListener("keyup", function () {
   if (!(email.value.includes("@") && email.value.includes(".com"))) {
     emailError.innerHTML = "올바른 메일 형식이 아닙니다!";
     emailError.style.color = "red";
+    isValid[2] = false;
   } else {
     emailError.innerHTML = "올바른 메일 형식입니다!";
     emailError.style.color = "green";
@@ -51,6 +57,7 @@ password.addEventListener("keyup", function () {
   if (!passwordPattern.test(password.value)) {
     passwordError.innerHTML = "영어+숫자+특수문자를 조합하여 작성해주세요.";
     passwordError.style.color = "red";
+    isValid[3] = false;
   } else {
     document.getElementById("passwordError").innerHTML =
       "안전한 비밀번호입니다!";
@@ -65,6 +72,7 @@ passwordCheck.addEventListener("keyup", function () {
   if (password.value !== passwordCheck.value) {
     passwordCheckError.style.color = "red";
     passwordCheckError.innerHTML = "비밀번호가 일치하지 않습니다.";
+    isValid[4] = false;
   } else {
     passwordCheckError.innerHTML = "비밀번호가 일치합니다.";
     passwordCheckError.style.color = "green";
@@ -74,7 +82,6 @@ passwordCheck.addEventListener("keyup", function () {
 });
 
 function checkSignUpButton() {
-  let button = document.getElementById("signUpButton");
   let hasErrors = true;
   for (let i = 0; i < isValid.length; i++) {
     if (isValid[i] === false) {
@@ -89,3 +96,13 @@ function checkSignUpButton() {
   }
   console.log(hasErrors);
 }
+
+button.addEventListener("click", function () {
+  if (button.style.backgroundColor === "rgba(0, 0, 0, 0.7)") {
+    modal.style.display = "flex";
+  }
+});
+
+closeButton.addEventListener("click", function () {
+  modal.style.display = "none";
+});
